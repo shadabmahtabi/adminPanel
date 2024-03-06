03c95b680dc4311aa09d8e558d1e0683fd0a1846
Shadab Mahtabi <shadabmanpuri@gmail.com>
2024-03-06 19:37:18 +0530

    admin

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,10 @@ var userSchema = mongoose.Schema({
   mobileNumber: Number,
   profession: String,
   password: String,
-  profilePic: String
+  profilePic: {
+    type: String,
+    default: 'https://images.unsplash.com/photo-1529068755536-a5ade0dcb4e8?q=80&w=1781&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
